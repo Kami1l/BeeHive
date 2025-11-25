1757a53734f63be012832c758d0bc0491d06937a
Kami1l <kamil.kozik44@gmail.com>
2025-11-25 23:09:38 +0100

    Dodanie opisu problemu do Config.java

--- src/ul/util/Config.java
+++ src/ul/util/Config.java
@@ -1,5 +1,16 @@
 package src.ul.util;
 
+// w ulu może znajdować się co najwyżej N/2 pszczól 
+// Rój liczy N osobników
+// Pszczoła która chce się dostac musi przejśc przez jedno z dwóch  istniejących przejść tylko jedna pszczoła może korzystać z przejścia w danym momencie
+// Pszczoła nie może przebywać w ulu zbyt długo bo się przegrzeje
+// W ulug przebywa królowa
+// Królowa składa jaja z których wykluwaja się pszczoły 
+// jaja zajmują tyle samo miejsca co pszczoła
+// Aby jaja zostały złożone musi byc miejsce w ulu
+// Program ma symulować cykl życia roju pszczół
+// robotnica umiera po pewnym określonym czasie X , liczonym ilością odwiedzin ula
+
 public class Config {
     
 }
